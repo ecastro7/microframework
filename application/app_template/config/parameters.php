85acccefbdec8c85a39014bdec1b23cc75c357e2
Eduard Castro <ecastro@telesurtv.net>
2014-11-10 18:08:12 -0430

    Ajuste de la estructura base

--- application/app_template/config/parameters.php
+++ application/app_template/config/parameters.php
@@ -1,5 +1,29 @@
 <?php
 
 /* * *****************************************************************************
-/*********************Configuracion ************************
-/******************************************************************************* */
+  /*********************Configuracion ************************
+  /******************************************************************************* */
+
+
+/** * Constantes para obtener las fecha actual y la siguiente. * */
+$today = date('Y/m/d', strtotime("2007/09/01"));
+define('TODAY', $today);
+$tomorrow = date('Y/m/d', strtotime("2008/01/01"));
+define('TOMORROW', $tomorrow);
+
+const SUBJECT_ANIVERSARIO = "FELIZ ANIVERSARIO";
+
+const NOT_ANIVERSARIO = "NO HAY ANIVERSARIO";
+
+const DESTINO_APLICACIONES = "ecastro@telesurtv.net";
+/*const FROM_TELESUR = "distribucion-alltelesur-2014@telesurtv.net";*/
+
+const DESTINO_TELESUR = "ecastro@telesurtv.net";
+
+$imgPATH = array(
+    "imgURL" => array(
+        "individual" => "http://exwebserv.telesurtv.net/Tarjetas/reconocimiento-individual.jpg"
+    )
+);
+
+define('URLIMG', serialize($imgPATH));
--- application/app_template/controllers/AppController.php
+++ application/app_template/controllers/AppController.php
@@ -1,13 +1,89 @@
 <?php
 
-namespace application\app_template\controllers {
+namespace application\app_aniversario\controllers {
 
-	require (getcwd().'/application/app_template/models/AppManager.php');
+    require ('application/app_aniversario/models/AppManager.php');
+    require ('application/libs/email/htmlMimeMail5/htmlMimeMail5.php');
 
-	use application\app_template\controllers\AppController as App;
-	
-	class AppController {
+    use application\app_aniversario\models\AppManager as Manager;
 
-	}
+    class AppController {
+
+        public static function getAniversario($fecha) {
+            return Manager::SearchNews($fecha);
+        }
+
+        public static function getListEmail() {
+            return Manager::SearchListEmail();
+        }
+
+        public static function renderInfo($result, $imgURL, $type, $subject, $Emails) {
+            $img = unserialize($imgURL);
+            if ($type == "INDIVIDUAL") {
+                $html = self::renderInfoIndividual($result, $img, $type, $subject, $Emails);
+            } else {
+                $html = self::renderInfoGeneral($result, $type, $subject);
+            }
+            return $html;
+        }
+
+        private static function renderInfoIndividual($result, $img, $type, $subject, $Emails) {
+            $img_path = $img["imgURL"]["individual"];
+            $html = array();
+            $render = array();
+            $diccionario = array(
+                'img_path' => $img_path,
+                'type_title' => "Felicitaciones" . $subject,
+                'type_table' => "individual"
+            );
+            foreach ($result as $key => $obj) {
+                $html = Manager::renderView(array($obj), $type);
+                $render[$key]["html"] = Manager::renderDinamicView($html, $diccionario);
+                $render[$key]["subject"] = $subject . " " . $obj->primer_nombre . " " . $obj->primer_apellido;
+                $render[$key]["to"] = self::getEmail($obj, $Emails);
+            }
+            return $render;
+        }
+
+        private static function renderInfoGeneral($result, $type, $subject) {
+            if ($type == "GENERAL" && count($result) > 8) {
+                $type_table = "generalDoble";
+            } else {
+                $type_table = "general";
+            }
+            $diccionario = array(
+                'type_title' => "Felicitaciones",
+                'type_table' => $type_table
+            );
+            $render = array();
+            $html = Manager::renderView($result, $type);
+            $render[0]["html"] = Manager::renderDinamicView($html, $diccionario);
+            $render[0]["subject"] = $subject;
+            $render[0]["to"] = "ecastro@telesurtv.net";
+            return $render;
+        }
+
+        private static function getEmail($obj, $Emails) {
+            foreach ($Emails as $mail) {
+                if ($mail->cedula == $obj->cedula) {
+//                    $to = $mail->username . "@telesurtv.net";
+                    $to = "ecastro@telesurtv.net";
+                }
+            }
+            return $to;
+        }
+
+        public static function EnviarMail($parametros) {
+//            print_r($parametros);
+            $mail = new \htmlMimeMail5();
+            $mail->setFrom("ecastro@telesurtv.net");
+            $mail->setSubject($parametros["subject"]);
+            $mail->setText($parametros["subject"]);
+            $mail->setHtml($parametros["html"]);
+            $mail->setSMTPParams('correo.telesurtv.net', 25, null, true, 'aplicaciones@telesurtv.net', '4pl1c4c10n35');
+            $mail->send(array($parametros["to"]), 'smtp');
+        }
+
+    }
 
 }
